1de28b5ea9b26bc372c6a6309c6f9149705fd5dc
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-02-04 18:18:46 +0300

    Update navigation.php

--- navigation.php
+++ navigation.php
@@ -1,22 +1,19 @@
 <?php
 
 return [
-
-    /*
-    'Getting Started -' => [
-        'url' => 'docs/getting-started',
+    'Getting Started' => [
         'children' => [
-            'Customizing Your Site' => 'docs/customizing-your-site',
-            'Navigation' => 'docs/navigation',
-            'Algolia DocSearch' => 'docs/algolia-docsearch',
-            'Custom 404 Page' => 'docs/custom-404-page',
+            'Introduction' => 'docs',
+            'Installation' => 'docs/installation',
+            'Quick Start'  => 'docs/quickstart',
         ],
     ],
-*/
-
-    'Introduction' => 'docs',
-    'Installation' => 'docs/installation',
-    'Quick Start' => 'docs/quickstart',
-    'Validation' => 'docs/custom-404-page',
-    'Authentication' => 'docs/customizing-your-site',
+    
+   'Features' => [
+        'children' => [
+            'Validation' => 'docs/custom-404-page',
+            'Authentication' => 'docs/customizing-your-site',
+            'Testing' => 'docs/customizing-your-site',
+        ],
+    ],
 ];
