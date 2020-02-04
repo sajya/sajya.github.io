33f7b64cdadb19417f2b24b027c1ad6cc11b32ff
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-02-04 18:22:39 +0300

    Update menu-item.blade.php

--- source/_nav/menu-item.blade.php
+++ source/_nav/menu-item.blade.php
@@ -8,7 +8,7 @@
         </a>
     @else
         {{-- Menu item without URL--}}
-        <p class="nav-menu__item text-gray-600">{{ $label }}</p>
+        <h4>{{ $label }}</h4>
     @endif
 
     @if (! is_string($item) && $item->children)
