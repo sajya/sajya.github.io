9cbe378f94d26ca3b3c8af481866f26715870743
Alexandr Chernyaev <bliz48rus@gmail.com>
2020-03-28 12:32:46 +0300

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -36,8 +36,6 @@
 
     @stack('meta')
 
-    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i"
-          rel="stylesheet">
     <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
 
     @if ($page->docsearchApiKey && $page->docsearchIndexName)
