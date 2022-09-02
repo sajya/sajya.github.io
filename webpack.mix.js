311450141d0a7042fd66508b45a461b8728e3447
tabuna <bliz48rus@gmail.com>
2022-09-02 04:50:54 +0300

    Fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,5 @@
 let mix = require('laravel-mix');
 let build = require('./tasks/build.js');
-require('laravel-mix-purgecss');
 
 mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build/');
@@ -20,9 +19,4 @@ mix.webpackConfig({
 mix.js('source/_assets/js/main.js', 'js')
     .sourceMaps()
     .sass('source/_assets/sass/main.scss', 'css/main.css')
-    //.purgeCss({
-    //    extensions: ['html', 'md', 'js', 'php', 'vue'],
-    //    folders: ['source'],
-    //    whitelistPatterns: [/language/, /hljs/, /algolia/],
-    //})
     .version();
