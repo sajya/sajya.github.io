4549c88824fa0c2ae4ae5c9cc950ca0b26386438
Alexandr Chernyaev <bliz48rus@gmail.com>
2023-08-06 03:52:42 +0300

    Update docs-menu.blade.php

--- resources/views/components/docs-menu.blade.php
+++ resources/views/components/docs-menu.blade.php
@@ -1,6 +1,6 @@
 <nav class="nav-docs px-md-5 py-2 px-3">
     @foreach($menu as $title => $values)
-        <h4 class="font-weight-normal my-md-3 my-2 me-2 me-md-0 d-none d-md-inline-block text-secondary">{{ $title }}</h4>
+        <h5 class="font-weight-normal my-md-3 my-2 me-2 me-md-0 d-none d-md-inline-block text-secondary">{{ $title }}</h5>
 
         <ul>
             @foreach($values as $title => $link)
